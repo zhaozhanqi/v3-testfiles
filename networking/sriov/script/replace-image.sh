c9f2051c637192e0e403024e8f72356eaec6d625
Zhanqi Zhao <zzhao@redhat.com>
2020-01-21 15:50:04 +0800

    Update replace-image.sh

--- networking/sriov/script/replace-image.sh
+++ networking/sriov/script/replace-image.sh
@@ -1,13 +1,20 @@
 oc get csv -o yaml > old-csv.yaml
 oc get is 4.3-art-latest -o yaml --config=/root/cikubeconfig | grep sriov | grep name: > new-image.yaml
+grep -i sriov new-image.yaml
+result=$?
+if [ $result -ne 0 ]; then
+  echo " CANNOT get the sriov image"
+  exit 1
+fi
 for i in ose-sriov-cni ose-sriov-dp-admission-controller ose-sriov-network-config-daemon ose-sriov-network-device-plugin ose-sriov-network-operator ose-sriov-network-webhook
-do 
+do
   dest=`cat new-image.yaml | grep -o "name: .*${i}" | sed 's/name: \(.*\)/\1/' | head -1`
   if [ ${i} != "ose-sriov-network-operator" ]; then
-     origin=`cat csv.yaml | grep -o "value:.*${i}.*" | sed 's/value: \(.*\)/\1/' | head -1`
+     origin=`cat old-csv.yaml | grep -o "value:.*${i}.*" | sed 's/value: \(.*\)/\1/' | head -1`
   else
-     origin=`cat csv.yaml | grep -o "image:.*${i}.*" | sed 's/image: \(.*\)/\1/' | head -1`
+     origin=`cat old-csv.yaml | grep -o "image:.*${i}.*" | sed 's/image: \(.*\)/\1/' | head -1`
   fi
   sed -i "s#$origin#$dest#g" old-csv.yaml
 done
-oc apply -f old-csv.yaml
+
+oc create -f old-csv.yaml
